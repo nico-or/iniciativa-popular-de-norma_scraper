42a6c361cba96b36d5126f12a49f9d0959806567
Nicolás Órdenes <nicordenes@gmail.com>
2022-07-19 14:27:05 -0400

    Add ChileConvencion#Iniciativa

---
+++ lib/chile_convencion/iniciativa.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module ChileConvencion
+  # Iniciativa Popular de Norma
+  class Iniciativa
+    attr_reader :id, :title, :comision, :topic, :publication_date,
+                :support_count, :type, :author, :organization, :url
+
+    BASE_URL = 'https://plataforma.chileconvencion.cl/m/iniciativa_popular/'
+
+    # Initialize with data from
+    #   - https://plataforma.chileconvencion.cl/m/iniciativa_popular/iniciativas.csv
+    #   - https://plataforma.chileconvencion.cl/m/iniciativa_indigena/iniciativas.csv
+    def initialize(csv_row)
+      @id, @title, @comision, @topic, @publication_date,
+      @support_count, @type, @author, @organization, @url = csv_row.fields
+    end
+  end
+end
