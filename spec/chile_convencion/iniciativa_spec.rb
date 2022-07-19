9cd9288ef067f8dc622d112f21bb90164151862a
Nicolás Órdenes <nicordenes@gmail.com>
2022-07-19 18:35:32 -0400

    Refactor rspec iniciativas fixtures

--- spec/chile_convencion/iniciativa_spec.rb
+++ spec/chile_convencion/iniciativa_spec.rb
@@ -1,21 +1,28 @@
 require 'csv'
 
 describe ChileConvencion::Iniciativa do
-  let(:iniciativa) do
-    entry = CSV.open('spec/fixtures/sample_iniciativa_popular.csv', headers: true)
+  def load_iniciativa(type)
+    entry = CSV.open("spec/fixtures/iniciativa_#{type}.csv", headers: true)
     described_class.new entry.first
   end
 
+  let(:iniciativa_aprobada) { load_iniciativa('aprobada') }
+  let(:iniciativa_rechazada) { load_iniciativa('rechazada') }
+
   describe '#details_url' do
     it do
-      url = 'https://plataforma.chileconvencion.cl/m/iniciativa_popular/detalle?id=71002'
+      iniciativa = iniciativa_aprobada
+      id = iniciativa.id
+      url = "https://plataforma.chileconvencion.cl/m/iniciativa_popular/detalle?id=#{id}"
       expect(iniciativa.details_url).to eq(url)
     end
   end
 
   describe '#stats_url' do
     it do
-      url = 'https://plataforma.chileconvencion.cl/m/iniciativa_popular/stats?id=71002'
+      iniciativa = iniciativa_aprobada
+      id = iniciativa.id
+      url = "https://plataforma.chileconvencion.cl/m/iniciativa_popular/stats?id=#{id}"
       expect(iniciativa.stats_url).to eq(url)
     end
   end
