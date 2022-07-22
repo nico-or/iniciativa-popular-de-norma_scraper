b7548378197e1711421f8366fc39b41f83c0154d
Nicolás Órdenes <nicordenes@gmail.com>
2022-07-22 17:14:31 -0400

    Add ChileConvencion.load_iniciativas helper method

--- lib/chile_convencion.rb
+++ lib/chile_convencion.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
+require 'csv'
+
 require_relative 'chile_convencion/iniciativa'
 require_relative 'chile_convencion/details_page'
 require_relative 'chile_convencion/stats_page'
 require_relative 'chile_convencion/script_tag'
+
+module ChileConvencion
+  # Load iniciativas CSV data
+  # Input:
+  #   - type: String (popular/indigena)
+  def self.load_iniciativas(type)
+    rows = CSV.open("./data/iniciativa_#{type}_norma.csv", headers: true)
+    rows.map { |row| ChileConvencion::Iniciativa.new(row, type) }
+  end
+end
