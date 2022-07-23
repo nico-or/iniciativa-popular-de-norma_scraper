752cc18d80c684c75e2ca416b7ad9be31858e1f8
Nicolás Órdenes <nicordenes@gmail.com>
2022-07-22 22:22:48 -0400

    Add Cabildo#details_url

--- lib/chile_convencion/cabildo.rb
+++ lib/chile_convencion/cabildo.rb
@@ -1,8 +1,20 @@
 module ChileConvencion
   # Cabildo o Encuentro
   class Cabildo
+    BASE_URL = 'https://plataforma.chileconvencion.cl/m/cabildos/'
+
     def initialize(nokogiri_element)
       @element = nokogiri_element
+    end
+
+    def details_url
+      URI.join(BASE_URL, relative_path).to_s
+    end
+
+    private
+
+    def relative_path
+      @element.css('a.boton').first.attr(:href)
     end
   end
 end
--- spec/chile_convencion/cabildo_spec.rb
+++ spec/chile_convencion/cabildo_spec.rb
@@ -5,4 +5,9 @@ describe ChileConvencion::Cabildo do
     card = Nokogiri(File.read('spec/fixtures/cabildo_card.html'))
     described_class.new(card)
   end
+
+  describe '#details_url' do
+    path = 'https://plataforma.chileconvencion.cl/m/cabildos/o/806'
+    it { expect(cabildo.details_url).to eq(path) }
+  end
 end
